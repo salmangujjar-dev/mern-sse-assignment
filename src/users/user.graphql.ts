55c8fdce8689d8046816f64621a6d61d1cca45e1
salmangujjar-dev <isalmandev@gmail.com>
2024-11-16 21:40:49 +0500

    chore: remove password from user object type

--- src/users/user.graphql.ts
+++ src/users/user.graphql.ts
@@ -1,21 +1,20 @@
 import {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLBoolean,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLBoolean,
 } from "graphql";
 
 const UserType = new GraphQLObjectType({
-    name: "User",
-    fields: () => ({
-        id: { type: GraphQLID },
-        email: { type: GraphQLString },
-        username: { type: GraphQLString },
-        password: { type: GraphQLString },
-        isAdmin: { type: GraphQLBoolean },
-        createdAt: { type: GraphQLString },
-        updatedAt: { type: GraphQLString },
-    }),
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLID },
+    email: { type: GraphQLString },
+    username: { type: GraphQLString },
+    isAdmin: { type: GraphQLBoolean },
+    createdAt: { type: GraphQLString },
+    updatedAt: { type: GraphQLString },
+  }),
 });
 
 export default UserType;
